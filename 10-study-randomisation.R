2a45509e338a63be620bae7fdd7a6357b629ce97
Thomas Klebel <t.klebel@hotmail.com>
2024-10-31 15:35:38 +0100

    replace TRUE/FALSE with yes/no and increase allocation sample size

--- 10-study-randomisation.R
+++ 10-study-randomisation.R
@@ -3,18 +3,18 @@ library(tidyverse)
 # following https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2267325/; figure 2
 block_lookup <- tribble(
   ~block_number, ~block_content,
-  1, c(TRUE, FALSE),
-  2, c(FALSE, TRUE),
-  3, c(TRUE, TRUE, FALSE, FALSE),
-  4, c(TRUE, FALSE, TRUE, FALSE),
-  5, c(TRUE, FALSE, FALSE, TRUE),
-  6, c(FALSE, TRUE, TRUE, FALSE),
-  7, c(FALSE, TRUE, FALSE, TRUE),
-  8, c(FALSE, FALSE, TRUE, TRUE)
+  1, c("yes", "no"),
+  2, c("no", "yes"),
+  3, c("yes", "yes", "no", "no"),
+  4, c("yes", "no", "yes", "no"),
+  5, c("yes", "no", "no", "yes"),
+  6, c("no", "yes", "yes", "no"),
+  7, c("no", "yes", "no", "yes"),
+  8, c("no", "no", "yes", "yes")
 )
 
-# set.seed()
-sample_indices <- sample(1:8, 250, replace = TRUE)
+set.seed(983745)
+sample_indices <- sample(1:8, 300, replace = TRUE)
 
 blocks <- tibble(block_number = sample_indices) %>% 
   left_join(block_lookup) %>% 
@@ -30,7 +30,7 @@ base_for_block_id <- tibble(block_number = sample_indices) %>%
   left_join(mutate(block_lookup, block_length = map_int(block_content, length))) %>% 
   select(block_number, block_length)
 
-block_id <- rep(1:250, times = base_for_block_id$block_length)
+block_id <- rep(1:300, times = base_for_block_id$block_length)
 
 blocks_cleaned <- blocks %>% 
   mutate(randomisation_id = seq_along(block_content),
@@ -38,4 +38,4 @@ blocks_cleaned <- blocks %>%
   select(randomisation_id, send_email = block_content,
          block_id)
 
-write_csv(blocks_cleaned, "sample_randomisation.csv")
+write_csv(blocks_cleaned, "study_randomisation.csv")
